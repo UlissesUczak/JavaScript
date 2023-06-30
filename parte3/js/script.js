56d8c6d922f765fee036df5890e997f2f89917cd
Ulisses U <uczakulisses@gmail.com>
2023-06-30 19:33:02 -0300

    exercicios basicos

---
+++ parte3/js/script.js
@@ -0,0 +1,16 @@
+//demonstração de case sensitive
+
+console.log("Primeiro");
+
+console.log ("Ultimo");
+
+
+
+
+
+//demonstração de tipagem fraca
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
---
+++ parte4/js/scripts.js
@@ -0,0 +1,12 @@
+//mudar o valor da variavel - ok, agora mudar o tipo é problema.
+
+nome = "João";
+
+
+var teste = 1;
+
+teste = 'Ulisses'
+
+console.log(teste);
+
+console.log(nome);

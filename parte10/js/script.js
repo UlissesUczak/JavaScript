6803a3e20442350f9434ff6f06a31c94f87c7fa2
Ulisses U <uczakulisses@gmail.com>
2023-07-03 17:29:06 -0300

    testes e exercicios

---
+++ parte10/js/script.js
@@ -0,0 +1,11 @@
+// array => []; //objetos => {}
+
+var arr = [5, "Ulisses", true, {teste: 1}, {teste: 2}];
+
+console.log(arr)
+console.log(arr[1])
+console.log(typeof arr)
+
+arr[8] = 23;
+
+console.log(arr)
---
+++ parte11-if/js/script.js
@@ -0,0 +1,7 @@
+var idade = 17;
+
+if(idade >= 18) {
+    console.log('Pode fazer a carteira de habilitação')
+} else {
+    console.log('Não pode fazer a carteira')
+}

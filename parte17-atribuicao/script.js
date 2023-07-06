758d0f92129d6e4a51613ed44b691d4b2568dee4
Ulisses U <uczakulisses@gmail.com>
2023-07-06 09:35:34 -0300

    exercicios

---
+++ parte17-atribuicao/script.js
@@ -0,0 +1,17 @@
+var x = 1;
+var y = 2;
+
+//soma 
+console.log(x = x + y);
+console.log(x += y)
+
+//subtração
+console.log(x -= y);
+
+//multiplicacao
+
+console.log(x *= y)
+
+//divisao
+
+console.log(x /= y);
